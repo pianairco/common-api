4f35595343be2086c9e3871a40992a3ded8c8f2b
rahmati <rahmati@jibit.ir>
2023-09-03 14:49:41 +0330

    responseCompleter

--- src/main/java/ir/piana/dev/common/handler/HandlerResponseBuilder.java
+++ src/main/java/ir/piana/dev/common/handler/HandlerResponseBuilder.java
@@ -8,21 +8,55 @@ import ir.piana.dev.jsonparser.json.JsonTarget;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.function.Consumer;
+
 @Component
 public class HandlerResponseBuilder {
     @Autowired
     private JsonParser jsonParser;
 
-    public <Res> HandlerResponse fromDto(Res responseDto) {
+    public HandlerResponseCompleter fromJsonTarget(JsonTarget jsonTarget) {
+        return new HandlerResponseCompleter(new HandlerResponseImpl(
+                jsonTarget.getJsonObject().toBuffer(), jsonTarget));
+    }
+
+    public <Res> HandlerResponseCompleter fromDto(Res responseDto) {
         JsonObject jsonObject = JsonObject.mapFrom(responseDto);
-        return new HandlerResponseImpl(jsonObject.toBuffer(), jsonParser.fromJson(jsonObject), responseDto);
+        return new HandlerResponseCompleter(new HandlerResponseImpl(
+                jsonObject.toBuffer(), jsonParser.fromJson(jsonObject), responseDto));
+    }
+
+    public class HandlerResponseCompleter {
+        private HandlerResponseBuilder.HandlerResponseImpl handlerResponse;
+
+        public HandlerResponseCompleter(HandlerResponseBuilder.HandlerResponseImpl handlerResponse) {
+            this.handlerResponse = handlerResponse;
+        }
+
+        public HandlerResponseBuilder.HandlerResponseCompleter setAdditionalParam(
+                Consumer<MapStrings.Appender> supplier) {
+            MapStrings.Builder consume = MapStrings.toConsume();
+            supplier.accept(consume);
+            handlerResponse.additionalParams = consume.build();
+            return this;
+        }
+
+        public HandlerResponseBuilder.HandlerResponseCompleter setAuthPhrase(
+                String authPhrase) {
+            handlerResponse.authPhrase = authPhrase;
+            return this;
+        }
+
+        public HandlerResponse build() {
+            return handlerResponse;
+        }
     }
 
     private static class HandlerResponseImpl<Res> implements HandlerResponse<Res> {
         private Buffer buffer;
         private JsonTarget jsonTarget;
         private Res dto;
-        private final MapStrings additionalParams = MapStrings.toProduce();
+        private MapStrings additionalParams = MapStrings.toProduce();
         private String authPhrase;
 
         public HandlerResponseImpl(Buffer buffer, JsonTarget jsonTarget) {
