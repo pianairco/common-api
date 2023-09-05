c4cc949ead1ebab8de4d97d67dbb58d255257974
rahmati <rahmati@jibit.ir>
2023-09-05 11:44:11 +0330

    some problem

--- src/main/java/ir/piana/dev/common/http/client/mock/MockHttpResponse.java
+++ src/main/java/ir/piana/dev/common/http/client/mock/MockHttpResponse.java
@@ -1,12 +1,43 @@
 package ir.piana.dev.common.http.client.mock;
 
-import lombok.Data;
+import io.vertx.core.buffer.Buffer;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Map;
 
-@Data
+@Setter
+@Getter
 public class MockHttpResponse {
-    private final int status;
-    private final String body;
-    private final Map<String, String> headers;
+    private int status;
+    @Setter(AccessLevel.NONE)
+    private String body;
+    @Setter(AccessLevel.NONE)
+    private Buffer bodyAsBuffer;
+    private Map<String, String> headers;
+
+    public void setBody(String body) {
+        this.body = body;
+        this.bodyAsBuffer = Buffer.buffer(body);
+    }
+
+    public MockHttpResponse() {
+
+    }
+
+    public MockHttpResponse(int status, String body, Map<String, String> headers) {
+        this.status = status;
+        this.body = body;
+        this.bodyAsBuffer = Buffer.buffer(body);
+        this.headers = headers;
+    }
+
+    public MockHttpResponse(int status, Buffer bodyAsBuffer, Map<String, String> headers) {
+        this.status = status;
+        this.body = bodyAsBuffer.toString();
+        this.bodyAsBuffer = bodyAsBuffer;
+        this.headers = headers;
+    }
 }
+
